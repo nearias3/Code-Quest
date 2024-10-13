5a58a7a397a037569d3c1dc5aa135d7c0f130097
nearias3 <127260408+nearias3@users.noreply.github.com>
2024-10-13 10:59:30 -0500

    Added placeholder code to all of the pages, imported ApolloProvider in the main.jsx file

---
+++ client/src/pages/GraphQLStage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const GraphQLStage = () => {
+  return (
+    <div>
+      <h2>GraphQL Stage</h2>
+      <p>Solve the challenges to move to the next stage!</p>
+      {/* Add the challenge logic here somewhere */}
+    </div>
+  );
+};
+
+export default GraphQLStage;
---
+++ client/src/pages/SqlStage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const SQLStage = () => {
+  return (
+    <div>
+      <h2>SQL Stage</h2>
+      <p>Solve the challenges to move to the next stage!</p>
+      {/* Add the challenge logic here somewhere */}
+    </div>
+  );
+};
+
+export default SQLStage;

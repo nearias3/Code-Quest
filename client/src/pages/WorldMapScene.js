4df4428e309af7da15940397ee00ae2b8814bdf6
Cneale92 <connorneale92@gmail.com>
2024-10-27 00:30:45 -0500

    made code changes to WorldMapScene to properly load the layers and assets for the tilemap

--- client/src/pages/WorldMapScene.js
+++ client/src/pages/WorldMapScene.js
@@ -9,9 +9,24 @@ class WorldMapScene extends Phaser.Scene {
 
   preload() {
     this.load.image(
-      "WorldMapSceneIsland",
-      "/assets/images/worldbuilding/WorldMapSceneIsland.png"
+      "02",
+      "/assets/images/worldbuilding/tilesets/02.png"
     );  
+    this.load.image("16", "/assets/images/worldbuilding/tilesets/16.png");
+    this.load.image("17", "/assets/images/worldbuilding/tilesets/17.png");
+    this.load.image("Bridge_All", "/assets/images/worldbuilding/tilesets/Bridge_All.png");    
+    this.load.image(
+      "Castle_Purple",
+      "/assets/images/worldbuilding/tilesets/Castle_Purple.png"
+    );
+    this.load.image("Goblin_House", "/assets/images/worldbuilding/tilesets/Goblin_House.png");
+    this.load.image("HappySheep_All", "/assets/images/worldbuilding/tilesets/HappySheep_All.png");
+    this.load.image("Rocks_04", "/assets/images/worldbuilding/tilesets/Rocks_04.png");  
+    this.load.image("Tilemap_Elevation", "/assets/images/worldbuilding/tilesets/Tilemap_Elevation.png"); 
+    this.load.image("Tilemap_Flat", "/assets/images/worldbuilding/tilesets/Tilemap_Flat.png"); 
+    this.load.image("Tree", "/assets/images/worldbuilding/tilesets/Tree.png");
+    this.load.image("W_Idle", "/assets/images/worldbuilding/tilesets/W_Idle.png");
+    this.load.image("Water", "/assets/images/worldbuilding/tilesets/Water.png");  
     this.load.tilemapTiledJSON(
       "WorldMapSceneIsland",
       "/assets/images/worldbuilding/WorldMapSceneIsland.json"
@@ -30,23 +45,36 @@ class WorldMapScene extends Phaser.Scene {
       console.error("Load and Save slots not passed correctly.");
     }
 
-    this.add.image(0, 0, "WorldMapSceneIsland");
+    this.add.image(0, 0, "02");
+    this.add.image(0, 0, "16");
+    this.add.image(0, 0, "17");
+    this.add.image(0, 0, "Bridge_All");
+    this.add.image(0, 0, "Castle_Purple");
+    this.add.image(0, 0, "Gobline_House");
+    this.add.image(0, 0, "HappySheep_All");
+    this.add.image(0, 0, "Rocks_04");
+    this.add.image(0, 0, "Tilemap_Elevation");
+    this.add.image(0, 0, "Tilemap_Flat");
+    this.add.image(0, 0, "Tree");
+    this.add.image(0, 0, "W_Idle");
+    this.add.image(0, 0, "Water");
+
     const map = this.make.tilemap({ key: "WorldMapSceneIsland" });
 
     // Add all the tilesets
-    const tileset1 = map.addTilesetImage("02", "WorldMapSceneIsland");
-    const tileset2 = map.addTilesetImage("16", "WorldMapSceneIsland");
-    const tileset3 = map.addTilesetImage("17", "WorldMapSceneIsland");
-    const tileset4 = map.addTilesetImage("Bridge_All", "WorldMapSceneIsland");
-    const tileset5 = map.addTilesetImage("Castle_Purple", "WorldMapSceneIsland");
-    const tileset6 = map.addTilesetImage("Goblin_House", "WorldMapSceneIsland");
-    const tileset7 = map.addTilesetImage("HappySheep_All", "WorldMapSceneIsland");
-    const tileset8 = map.addTilesetImage("Rocks_03", "WorldMapSceneIsland");
-    const tileset9 = map.addTilesetImage("Tilemap_Elevation", "WorldMapSceneIsland");
-    const tileset10 = map.addTilesetImage("Tilemap_Flat", "WorldMapSceneIsland");
-    const tileset11 = map.addTilesetImage("Tree", "WorldMapSceneIsland");
-    const tileset12 = map.addTilesetImage("W_Idle", "WorldMapSceneIsland");
-    const tileset13 = map.addTilesetImage("Water", "WorldMapSceneIsland");
+    const tileset1 = map.addTilesetImage("02", "02");
+    const tileset2 = map.addTilesetImage("16", "16");
+    const tileset3 = map.addTilesetImage("17", "17");
+    const tileset4 = map.addTilesetImage("Bridge_All", "Bridge_All");
+    const tileset5 = map.addTilesetImage("Castle_Purple", "Castle_Purple");
+    const tileset6 = map.addTilesetImage("Goblin_House", "Goblin_House");
+    const tileset7 = map.addTilesetImage("HappySheep_All", "HappySheep_All");
+    const tileset8 = map.addTilesetImage("Rocks_04", "Rocks_04");
+    const tileset9 = map.addTilesetImage("Tilemap_Elevation", "Tilemap_Elevation");
+    const tileset10 = map.addTilesetImage("Tilemap_Flat", "Tilemap_Flat");
+    const tileset11 = map.addTilesetImage("Tree", "Tree");
+    const tileset12 = map.addTilesetImage("W_Idle", "W_Idle");
+    const tileset13 = map.addTilesetImage("Water", "Water");
 
 
     // Create layers

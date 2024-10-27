2af2f660af46428b61a3f3d9b5d289efd51e54eb
Cneale92 <connorneale92@gmail.com>
2024-10-27 00:06:57 -0500

    made changes to the WorldMap json and the WorldMap png to get the map to finally load

--- client/src/pages/WorldMapScene.js
+++ client/src/pages/WorldMapScene.js
@@ -8,11 +8,14 @@ class WorldMapScene extends Phaser.Scene {
   }
 
   preload() {
+    this.load.image(
+      "WorldMapSceneIsland",
+      "/assets/images/worldbuilding/WorldMapSceneIsland.png"
+    );  
     this.load.tilemapTiledJSON(
       "WorldMapSceneIsland",
-      "/assets/images/worldbuilding/WorldMapSceneIsland.tmj"
+      "/assets/images/worldbuilding/WorldMapSceneIsland.json"
     );
-    this.load.image("door", "/assets/door.png");
     this.load.image("placeholderCharacter", "/assets/placeholderCharacter.png");
   }
 
@@ -27,7 +30,41 @@ class WorldMapScene extends Phaser.Scene {
       console.error("Load and Save slots not passed correctly.");
     }
 
+    this.add.image(0, 0, "WorldMapSceneIsland");
     const map = this.make.tilemap({ key: "WorldMapSceneIsland" });
+
+    // Add all the tilesets
+    const tileset1 = map.addTilesetImage("02", "WorldMapSceneIsland");
+    const tileset2 = map.addTilesetImage("16", "WorldMapSceneIsland");
+    const tileset3 = map.addTilesetImage("17", "WorldMapSceneIsland");
+    const tileset4 = map.addTilesetImage("Bridge_All", "WorldMapSceneIsland");
+    const tileset5 = map.addTilesetImage("Castle_Purple", "WorldMapSceneIsland");
+    const tileset6 = map.addTilesetImage("Goblin_House", "WorldMapSceneIsland");
+    const tileset7 = map.addTilesetImage("HappySheep_All", "WorldMapSceneIsland");
+    const tileset8 = map.addTilesetImage("Rocks_03", "WorldMapSceneIsland");
+    const tileset9 = map.addTilesetImage("Tilemap_Elevation", "WorldMapSceneIsland");
+    const tileset10 = map.addTilesetImage("Tilemap_Flat", "WorldMapSceneIsland");
+    const tileset11 = map.addTilesetImage("Tree", "WorldMapSceneIsland");
+    const tileset12 = map.addTilesetImage("W_Idle", "WorldMapSceneIsland");
+    const tileset13 = map.addTilesetImage("Water", "WorldMapSceneIsland");
+
+
+    // Create layers
+    map.createLayer("Water", tileset13);
+    map.createLayer("Sand", tileset10);
+    map.createLayer("ElevatedTerrain", tileset9);
+    map.createLayer("Grass", tileset10);
+    map.createLayer("Bridges", tileset4);
+    map.createLayer("Castle", tileset5);
+    map.createLayer("Building", tileset6);
+    map.createLayer("Trees", tileset11);
+    map.createLayer("WaterRocks", tileset8);
+    map.createLayer("SkullSign", tileset2);
+    map.createLayer("ArrowSign", tileset3);
+    map.createLayer("Sheep", tileset7);
+    map.createLayer("Mushrooms", tileset1);
+    map.createLayer("Wood", tileset12);
+
 
     this.add
       .text(400, 50, "Dimension of Magic", { fontSize: "32px", fill: "#fff" })
